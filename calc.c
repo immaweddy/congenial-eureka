2fda004a52ed106df2d08b889f4e764543204505
immaweddy <immakinoti200@gmail.com>
2024-05-13 12:08:35 +0300

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+int add (num1, num2);
+int subtract(num1,num2);
+{char operation;
+int num1 , num2 , result;
+printf("Enter an operation (+ or_):");
+scanf("%d", operation);
+
+printf("Enter two numbers(num1 or num2):");
+scanf ("%d",& num1,num2);
+}
+return 0;
+}
